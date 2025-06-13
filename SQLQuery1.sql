12f28a1694002a6e75798d1804f3256e1b0422df
duyhao2025 <thangnm222@uef.edu.vn>
2025-06-13 22:22:45 +0700

    Update database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE WEB_EVENT;
+CREATE DATABASE WEB_EVENT
 GO
 
 USE WEB_EVENT;
 GO
 
-CREATE TABLE NguoiDung (
+CREATE TABLE users  (
     ma_nguoi_dung INT PRIMARY KEY IDENTITY(1,1),
     ho_ten NVARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -12,21 +12,20 @@ CREATE TABLE NguoiDung (
     so_dien_thoai VARCHAR(20),
     xac_thuc BIT DEFAULT 0,
     ngon_ngu VARCHAR(5) DEFAULT 'vi',
-    vai_tro NVARCHAR(20) 
-      CHECK (vai_tro IN ('nguoi_tham_gia', 'nguoi_to_chuc')) 
-      DEFAULT 'nguoi_tham_gia'
+    vai_tro NVARCHAR(20) CHECK (vai_tro IN ('nguoi_tham_gia', 'nguoi_to_chuc')) DEFAULT 'nguoi_tham_gia'
 );
-GO
+
 
 CREATE TABLE DanhMuc (
     ma_danh_muc INT PRIMARY KEY IDENTITY(1,1),
     ten_vi NVARCHAR(100),
     ten_en NVARCHAR(100)
 );
-GO
+
 
 CREATE TABLE SuKien (
     ma_su_kien INT PRIMARY KEY IDENTITY(1,1),
+	ma_nguoi_dung INT,
     tieu_de NVARCHAR(200) NOT NULL,
     mo_ta NVARCHAR(MAX),
     hinh_anh VARCHAR(255),
@@ -42,11 +41,10 @@ CREATE TABLE SuKien (
     ma_nguoi_to_chuc INT,
     trang_thai VARCHAR(50) DEFAULT 'upcoming',
     thong_tin_ngan_hang VARCHAR(255),
-    ma_nguoi_dung INT,
     FOREIGN KEY (ma_danh_muc) REFERENCES DanhMuc(ma_danh_muc),
-    FOREIGN KEY (ma_nguoi_dung) REFERENCES NguoiDung(ma_nguoi_dung)
+    FOREIGN KEY (ma_nguoi_dung) REFERENCES users(ma_nguoi_dung)
 );
-GO
+
 
 CREATE TABLE DangKy (
     ma_dang_ky INT PRIMARY KEY IDENTITY(1,1),
@@ -57,46 +55,43 @@ CREATE TABLE DangKy (
     da_checkin BIT DEFAULT 0,
     thoi_gian_checkin DATETIME,
     da_huy BIT DEFAULT 0,
-    FOREIGN KEY (ma_nguoi_dung) REFERENCES NguoiDung(ma_nguoi_dung),
+    FOREIGN KEY (ma_nguoi_dung) REFERENCES users (ma_nguoi_dung),
     FOREIGN KEY (ma_su_kien) REFERENCES SuKien(ma_su_kien)
 );
-GO
+
 
 CREATE TABLE DanhGia (
-    danh_gia_id INT PRIMARY KEY IDENTITY(1,1),
+	danh_gia_id INT PRIMARY KEY IDENTITY(1,1),
+	ma_nguoi_dung INT,
+	ma_su_kien INT,
     nguoi_dung_id INT,
-    ma_nguoi_dung INT,
-    ma_su_kien INT,
     su_kien_id INT,
     diem INT CHECK (diem BETWEEN 1 AND 5),
     nhan_xet NVARCHAR(MAX),
     ngay_danh_gia DATETIME DEFAULT GETDATE(),
-    FOREIGN KEY (ma_nguoi_dung) REFERENCES NguoiDung(ma_nguoi_dung),
-    FOREIGN KEY (ma_su_kien) REFERENCES SuKien(ma_su_kien)
+    FOREIGN KEY (ma_nguoi_dung) REFERENCES users(ma_nguoi_dung),
+   FOREIGN KEY (ma_su_kien) REFERENCES SuKien(ma_su_kien)
 );
-GO
+
 
 CREATE TABLE YeuCauToChuc (
     ma_yeu_cau INT PRIMARY KEY IDENTITY(1,1),
-    ma_nguoi_dung INT,
+    
+	ma_nguoi_dung INT,
     ngay_yeu_cau DATETIME DEFAULT GETDATE(),
     trang_thai VARCHAR(50) DEFAULT 'pending',
     ghi_chu NVARCHAR(255),
-    FOREIGN KEY (ma_nguoi_dung) REFERENCES NguoiDung(ma_nguoi_dung)
+    FOREIGN KEY (ma_nguoi_dung) REFERENCES [users](ma_nguoi_dung)
 );
-GO
 
 CREATE TABLE DiaDiemToChuc (
     ma_su_kien INT PRIMARY KEY,
     dia_chi NVARCHAR(MAX),
     FOREIGN KEY (ma_su_kien) REFERENCES SuKien(ma_su_kien)
 );
-GO
 
 CREATE TABLE NgonNguDaiDien (
     ma_nguoi_dung INT PRIMARY KEY,
-    ngon_ngu NVARCHAR(10) 
-      CHECK (ngon_ngu IN ('vi', 'en')) DEFAULT 'vi',
-    FOREIGN KEY (ma_nguoi_dung) REFERENCES NguoiDung(ma_nguoi_dung)
+    ngon_ngu NVARCHAR(10) CHECK (ngon_ngu IN ('vi', 'en')) DEFAULT 'vi',
+    FOREIGN KEY (ma_nguoi_dung) REFERENCES users(ma_nguoi_dung)
 );
-GO
